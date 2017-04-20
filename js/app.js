29aa6c1443e578d9dc33c376fa18ae86ca2caf56
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-20 18:36:03 -0500

    inicio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var contenedor= document.getElementsByClassName("container");
+var cabecera = document.createElement("div");
+cabecera.setAttribute("id","cabecera");
+
+var informacion = document.createElement("div");
+informacion.setAttribute("id","informacion");
+
+var footer = document.createElement("div");
+footer.setAttribute("id","footer");
