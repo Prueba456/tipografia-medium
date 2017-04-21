43605c6275b2f6fb3f510aaac51ecd3dcef425f0
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-21 10:17:03 -0500

    armando estruactura

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-var contenedor= document.getElementsByClassName("container");
+var contenedor= document.getElementsByClassName("container")[0];
 var cabecera = document.createElement("div");
 cabecera.setAttribute("id","cabecera");
+var twitter = document.createElement("span");
+twitter.setAttribute("class","icon-twitter" );
+
+
+
 
 var informacion = document.createElement("div");
 informacion.setAttribute("id","informacion");
 
 var footer = document.createElement("div");
 footer.setAttribute("id","footer");
+
+contenedor.appendChild(cabecera);
+contenedor.appendChild(informacion),
+contenedor.appendChild(footer);
+cabecera.appendChild(twitter);
